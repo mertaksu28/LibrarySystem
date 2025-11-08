145c2a0105558392e4036152e7bd83015c1aa282
mertaksu <mertaksu458@gmail.com>
2025-11-08 00:58:16 -0500

    add book method and list book method

---
+++ src/Library.java
@@ -0,0 +1,23 @@
+public class Library {
+    Book[] books = new Book[10];
+    int bookCount = 0;
+    int borrowCount = 0;
+
+    void addBook(Book book) {
+        if (bookCount < books.length) {
+            books[bookCount] = book;
+            bookCount++;
+            IO.println(book.title + " added to library");
+        } else {
+            System.out.println("Library full you can't add book");
+        }
+    }
+
+    void listBooks() {
+        for (int i = 0; i < bookCount; i++) {
+            books[i].printInfo();
+        }
+        System.out.println("Total book count " + bookCount);
+        IO.println("Total borrowwed count " + borrowCount);
+    }
+}
